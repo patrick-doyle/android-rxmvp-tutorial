c6090d5f9910cb3b1fa9127824bf81425af95ec6
Patrick Doyle <patrick@twistedequations.com>
2017-01-29 17:42:36 +0000

    End of video 1

--- app/src/main/java/com/twistedeqations/rxmvp/sample/activities/home/MainActivity.java
+++ app/src/main/java/com/twistedeqations/rxmvp/sample/activities/home/MainActivity.java
@@ -2,6 +2,8 @@ package com.twistedeqations.rxmvp.sample.activities.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import com.twistedeqations.rxmvp.sample.R;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/twistedeqations/rxmvp/sample/app/dagger/AppComponent.java
+++ app/src/main/java/com/twistedeqations/rxmvp/sample/app/dagger/AppComponent.java
@@ -1,13 +1,24 @@
 package com.twistedeqations.rxmvp.sample.app.dagger;
 
 import android.content.Context;
+import com.squareup.picasso.Picasso;
 import com.twistedeqations.rxmvp.sample.app.dagger.module.AppModule;
+import com.twistedeqations.rxmvp.sample.app.dagger.module.GsonModule;
+import com.twistedeqations.rxmvp.sample.app.dagger.module.NetworkModule;
+import com.twistedeqations.rxmvp.sample.app.network.GithubNetwork;
 import dagger.Component;
+import okhttp3.OkHttpClient;
 
 @AppScope
-@Component(modules = { AppModule.class })
+@Component(modules = { AppModule.class , NetworkModule.class, GsonModule.class})
 public interface AppComponent {
 
   Context context();
+
+  OkHttpClient okhttpClient();
+
+  GithubNetwork githubNetwork();
+
+  Picasso picasso();
 
 }
